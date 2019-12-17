5f1b2d1a3476d84fbf71632625fc6d155275024a
jauzzz <18814128016@163.com>
2019-12-17 16:55:39 +0800

    modify max_length of hashchar field

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -318,7 +318,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     # email = models.EmailField(_('email address'), blank=True)
-    email = models.HashCharField(_('email address'), max_length=256, blank=True)
+    email = models.HashCharField(_('email address'), max_length=255, blank=True)
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
